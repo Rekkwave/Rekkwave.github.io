92ea88648dd0c544cc0334aa4c22a00a2a3f53dd
Rekkwave <isaacpulido2399@gmail.com>
2019-10-28 20:27:25 -0600

    poiupoiu

--- lesson4/scripts/main.js
+++ lesson4/scripts/main.js
@@ -1,10 +1,11 @@
-var currentDate = new Date();
-var currentDateString;
+let currentDate = new Date();
+
+let currentDateString;
+
 
 // get day of week
-var weekDayNumber = currentDate.getDay();
-
-var daysOfWeek =[
+let weekDayNumber = currentDate.getDay();
+let daysOfWeek =[
     'Sunday',
     'Monday',
     'Tuesday',
@@ -13,14 +14,10 @@ var daysOfWeek =[
     'Friday',
     'Saturday'
 ];
-
-var weekDay = daysOfWeek[weekDayNumber];
-
+let weekDay = daysOfWeek[weekDayNumber];
 currentDateString = weekDay + ', ';
-
 currentDateString  = currentDateString + currentDate.getDate();
-
-var months =[
+let months =[
     'January',
     'February',
     'March',
@@ -34,19 +31,35 @@ var months =[
     'November',
     'December',
 ];
-
-var monthNumber = currentDate.getMonth();
-var month = months[monthNumber];
-
+let monthNumber = currentDate.getMonth();
+let month = months[monthNumber];
 currentDateString += ' ' + month;
-
 currentDateString += ' ' + currentDate.getFullYear();
-
 document.getElementById('currentDate').innerHTML = currentDateString;
-//Debug:
-
 console.log(currentDateString);
-
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+//create 5 day forecast
+let currentDay = weekDayNumber;
+
+for (let i = 1; i < 6; i++) {
+    // add one day to current day
+    currentDay++;
+
+    // if current day is greater than 6 (Saturday), reset it to 0 (Sunday)
+    if (currentDay > 6) {
+        currentDay = 0;
+    }
+
+    // assing value to placeholder in HTML
+    const element = document.getElementById(`day${i}`);
+
+    element.innerHTML = daysOfWeek[currentDay];
+
+    // show/hide pancake
+    if (weekDayNumber == 5){
+        document.getElementById("pancake").removeAttribute("class", "hidden");
+    }
+    
 }
---
+++ lesson6/scripts/windchill.js
@@ -0,0 +1,18 @@
+let currentTemp = 41;
+let windSpeed = 8;
+let windChill = 0;
+
+
+if (currenttemp < 50 && windSpeed > 3) {
+let windChill = 35.74 + 0.6215 * currentTemp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * currentTemp + Math.pow(windSpeed, 0.16);
+}
+document.getElementById("currentTemp").innerHTML = currentTemp + '&degF';
+document.getElementById("windSpeed").innerHTML = windSpeed + ' mph';
+
+
+if (windChill === 0) {
+document.getElementById("windChill").innerHTML = 'N/A';
+}
+else {
+    document.getElementById("windChill").innerHTML = Math.round(windChill) + '&degF';
+}
