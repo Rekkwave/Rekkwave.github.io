1b952f023eb93cb3a9badb0335a453ea975c1f8c
Rekkwave <isaacpulido2399@gmail.com>
2019-11-16 16:03:14 -0700

    oiu

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -8,15 +8,82 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
  //   console.table(jsonObject);  // temporary checking for valid response and data parsing
 //  });
 
-  fetch(requestURL)
-    .then(response => {
-        response.json()
-        .then(
-            response => {
-                console.table(response.prophets);
+  // fetch(requestURL)
+  //   .then(response => {
+  //       response.json()
+  //       .then(
+  //           response => {
+  //               console.table(response.prophets);
                 
-                let prophet = response.prophets[0];
+  //               let prophet = response.prophets[0];
 
-                console.log(prophet.name);
-            });
-    });
+  //               console.log(prophet.name);
+  //           });
+  //   });
+    fetch(requestURL)
+     .then(response => {
+       response.json()
+       .then(
+         response => {
+           const prophets = response.prophets;
+
+           prophets.forEach(
+             (prophet) => {
+               //DEBUG:
+               console.log(prophet);
+
+              let article = document.createElement('article');
+              let h2 = document.createElement('h2');
+              
+              h2.textContent = prophet.name + ' ' + prophet.lastname;
+              
+              article.appendChild(h2);
+
+              // add date of birth
+              let birthdateDiv = document.createElement('div');
+
+              let birthdateLabel = document.createElement('label');
+              birthdateLabel.textContent = 'Date of Birth:';
+
+              let birthdateSpan = document.createElement('span');
+              birthdateSpan.textContent = prophet.birthdate;
+
+              article.appendChild(birthdateLabel);
+              article.appendChild(birthdateSpan);
+
+              article.appendChild(birthdateDiv);
+
+
+              //add place of birth
+
+              let birthplaceDiv = document.createElement('div');
+              
+              let birthplaceLabel = document.createElement('label');
+              birthplaceLabel.textContent = 'Place of Birth:';
+
+              let birthplaceSpan = document.createElement('span');
+              birthplaceSpan.textContent = prophet.birthplace;
+
+              article.appendChild(birthplaceLabel);
+              article.appendChild(birthplaceSpan);
+
+              article.appendChild(birthplaceDiv);
+
+              // add image
+
+              let imageDiv = document.createElement('div');
+              let image = document.createElement('img');
+
+              image.setAttribute('src', prophet.imageurl);
+              image.setAttribute('alt', prophet.name + ' ' + prophet.lastname + ' - ' + prophet.order);
+              
+              imageDiv.appendChild(image);
+
+              article.appendChild(imageDiv);
+
+              document.querySelector('section.prophets').appendChild(article);
+             }
+            ); 
+         });
+     });
+     
---
+++ lesson9/scripts/towndata.js
@@ -0,0 +1,33 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+.then(response => {
+  response.json()
+  .then(
+    response => {
+      const towns = response.towns;
+
+      towns.forEach(
+          (town) => {
+              if (town.name.toLowerCase() === 'fish haven') {
+                  // Fish Haven
+                  document.querySelector('#fishHavenYearFounded')
+                  .textContent = town.yearFounded; //add other elements
+
+                  document.querySelector('#fishHavenImage')
+                    .setAttribute('src', 'images/', town.photo);
+
+              } else if (town.name.toLowerCase() === 'preston') {
+                  // Preston
+                  document.querySelector('#prestonYearFounded')
+                  .textContent = town.yearFounded;
+              } 
+              else (town.name.toLowerCase() === 'soda springs') {
+                  // Soda Springs
+                  document.querySelector('#sodaSpringsYearFounded')
+                    .textContent = town.yearFounded;
+              }
+          }
+      );
+    })
+});
