f8ee8f2a60962f557f6a883bc5a131459030a7d7
Rekkwave <isaacpulido2399@gmail.com>
2019-12-02 09:36:04 -0700

    ijij

--- lesson10/scripts/five-day-forecast.js
+++ lesson10/scripts/five-day-forecast.js
@@ -1,38 +1,45 @@
- //get day of week
-let weekDayNumber = currentDate.getDay();
-let daysOfWeek =[
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-];
-let weekDay = daysOfWeek[weekDayNumber];
-currentDateString = weekDay + ', ';
-currentDateString  = currentDateString + currentDate.getDate();
-let months =[
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-];
-let monthNumber = currentDate.getMonth();
-let month = months[monthNumber];
-currentDateString += ' ' + month;
-currentDateString += ' ' + currentDate.getFullYear();
-document.getElementById('currentDate').innerHTML = currentDateString;
-console.log(currentDateString);
+function getFiveDayForecast(cityId) {
+    var weekDay = daysOfWeek[weekDayDate];
 
+    todayString = weekDay + ", ";
 
-//create 5 day forecast
-let currentDay = weekDayNumber;
+    //  function getWeather
+    // const apiForecastURL = 'https://api.openweathermap.org/data/2.5/forecast/?id=' + cityId + '&appid=a4de96eb4c9cdd8c0998596fbb5ae04d&units=imperial';
+
+    fetch(apiForecastURL)
+        .then(
+            (response) => response.json()
+        )
+        .then(
+            (forecasts) => {
+
+                let hourString = "18:00:00";
+                let counter = 1;
+
+                //loop through results
+                forecasts.list.forEach(
+                    (forecast) => {
+
+                        if (forecast.dt_txt.includes(hourString)) {
+
+                            // get temp
+                            const element = document.getElementById(`temp${counter}`);
+                            element.innerHTML = forecast.main.temp + '&deg';
+
+                            // get day of week
+                            let tomorrowDate = new Date(forecast.dt_txt);
+
+                            const dayElement = document.getElementById(`day${counter}`);
+                            dayElement.innerHTML = daysOfWeek[tomorrowDate.getDay()];
+
+                            // get image
+                            const imageElement = document.getElementById(`img${counter}`);
+                            imageElement.setAttribute('src', 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + ".png");
+                            imageElement.setAttribute('alt', forecast.weather[0].main);
+                            counter += 1;
+                        }
+                    }
+                )
+
+            });
+}
