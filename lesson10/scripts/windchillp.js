2aec178f71896e411c81e16e9c74cdf4b9f7fe20
Rekkwave <isaacpulido2399@gmail.com>
2019-12-02 19:35:42 -0700

    done

--- lesson10/scripts/windchillp.js
+++ lesson10/scripts/windchillp.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=2516eba340c4116d3940474f9040b5a2&units=imperial";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=2516eba340c4116d3940474f9040b5a2&units=imperial";
 
 fetch(apiURL)
     .then(
