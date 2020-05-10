966bf6fdf8ce8764dc0c67d1fa78b9db78d3c1a8
Rekkwave <isaacpulido2399@gmail.com>
2020-05-09 21:27:20 -0700

    iy

--- CoursePortalHomePage/scripts/currentDate.js
+++ CoursePortalHomePage/scripts/currentDate.js
@@ -1,3 +1,8 @@
 var currentDate = new Date();
 var currentYear = currentDate.getFullYear();
 document.getElementById("currentYear").innerHTML = currentYear;
+
+// var dateModified = currentDate.getMinutes();
+// document.getElementById("dateModified1").innerHTML = dateModified1;
+// var dateModified = currentDate.getSeconds();
+// document.getElementById("dateModified").innerHTML = dateModified;
--- lesson5/scripts/main.js
+++ lesson5/scripts/main.js
@@ -1,12 +1,7 @@
 let currentDate = new Date();
-<<<<<<< HEAD
-let currentDateString;
-=======
-
 let currentDateString;
 
 
->>>>>>> 356ac8e5caf4d316ff9298991f5cf16b5ac1ff89
 // get day of week
 let weekDayNumber = currentDate.getDay();
 let daysOfWeek =[
@@ -21,30 +16,9 @@ let daysOfWeek =[
 let weekDay = daysOfWeek[weekDayNumber];
 currentDateString = weekDay + ', ';
 currentDateString  = currentDateString + currentDate.getDate();
-let months =[
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-];
-let monthNumber = currentDate.getMonth();
-let month = months[monthNumber];
-currentDateString += ' ' + month;
-currentDateString += ' ' + currentDate.getFullYear();
-document.getElementById('currentDate').innerHTML = currentDateString;
-console.log(currentDateString);
-<<<<<<< HEAD
-function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+
+
+
 //creates 5 day forecast
 let day1, day2, day3, day4, day5;
 if (weekDayNumber < 6) {
@@ -55,33 +29,37 @@ if (weekDayNumber < 6) {
     document.getElementById("day4").innerHTML = day4;
     document.getElementById("day5").innerHTML = day5;
 
-=======
 
-function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
-
-//create 5 day forecast
 let currentDay = weekDayNumber;
 
 for (let i = 1; i < 6; i++) {
     // add one day to current day
     currentDay++;
-
     // if current day is greater than 6 (Saturday), reset it to 0 (Sunday)
     if (currentDay > 6) {
         currentDay = 0;
     }
 
-    // assing value to placeholder in HTML
-    const element = document.getElementById(`day${i}`);
+    // Asigning value to placeholder in HTML
+    const ent = document.getElementById(`day${i}`);
 
     element.innerHTML = daysOfWeek[currentDay];
+// show/hide pancake
+let weekDayNumber = currentDate.getDay();
+let daysOfWeek =[
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
 
-    // show/hide pancake
+
+
+let weekDay = daysOfWeek[weekDayNumber];
     if (weekDayNumber == 5){
         document.getElementById("pancake").removeAttribute("class","hidden");
     }
-    
->>>>>>> 356ac8e5caf4d316ff9298991f5cf16b5ac1ff89
-}
+}}
